a6eb9d4b9f6579a35d50ceb9b899f71652532dc4
Tyujhty <fannybaquey@gmail.com>
2023-12-27 10:00:42 +0000

    feat: add if statement to choose which function you want to call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -9,11 +9,20 @@ def encrypt(text, shift):
     new_encode_text = ''
     for letter in encode_text:
         if letter in alphabet:
-            new_letter = alphabet[alphabet.index(letter) + shift]
-            new_encode_text += new_letter
+            new_encode_text += alphabet[alphabet.index(letter) + shift]
     print(f"The encode message is: {new_encode_text}")
-            
-encrypt(text, shift)
 
+def decrypt(text, shift):
+    decode_text = list(text)
+    new_decode_text = ''
+    for letter in decode_text:
+        if letter in alphabet:
+            new_decode_text += alphabet[alphabet.index(letter) - shift]
+    print(f"The decode message is: {new_decode_text}")
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+if direction == 'encode':
+    encrypt(text, shift)
+elif direction == 'decode':
+    decrypt(text, shift)
+else:
+    print("Please, choose a correct direction")
