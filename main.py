bb69acd0b63a5032e5550f9be057ee4da6ad8c41
Tyujhty <fannybaquey@gmail.com>
2023-12-27 09:34:17 +0000

    bug: problem with z letter, solve by duplicating the letters of alphabet list

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
@@ -6,14 +6,14 @@ shift = int(input("Type the shift number:\n"))
 
 def encrypt(text, shift):
     encode_text = list(text)
+    new_encode_text = ''
     for letter in encode_text:
         if letter in alphabet:
-            new_encode_text = alphabet[alphabet.index(letter) + shift]
-            print(new_encode_text)
+            new_letter = alphabet[alphabet.index(letter) + shift]
+            new_encode_text += new_letter
+    print(f"The encode message is: {new_encode_text}")
             
 encrypt(text, shift)
 
 
     ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
