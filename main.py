121e8043a87d02d5816c50db87e7ae9353f37da4
Tyujhty <fannybaquey@gmail.com>
2023-12-27 15:25:37 +0000

    feat: Use of isalpha() on the letter.

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ alphabet = [
     'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
     't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
+def caesar(direction, text, shift):
+    cypher_text = ''
+    if direction == 'decode':
+        shift *= -1
+    for letter in text:
+        isalpha_letter = letter.isalpha()
+        if isalpha_letter is True:
+            letter_position = alphabet.index(letter)
+            cypher_text += alphabet[letter_position + shift]
+        else:
+            cypher_text += letter
+    print(f"The {direction} message is: {cypher_text}\n")
 
 print(logo)
 play_again = True
@@ -13,21 +25,11 @@ play_again = True
 while play_again == True:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
-    shift = int(input("Type the shift number:\n"))
     #modulus to set the range
-    shift = shift % 26
-
-    def caesar(direction, text, shift):
-        cypher_text = ''
-        if direction == 'decode':
-            shift *= -1
-        for letter in text:
-            letter_position = alphabet.index(letter)
-            cypher_text += alphabet[letter_position + shift]
-        print(f"The {direction} message is: {cypher_text}\n")
-
+    shift = int(input("Type the shift number:\n")) % 26
+    
     caesar(direction, text, shift)
     play_again = input("Type 'Yes' if you want to go again. Otherwise type 'no'").lower()
+    
     if play_again == 'yes':
         play_again = True
-
