bef32d290008e06ae7373105688e3a77a1431dd9
Tyujhty <fannybaquey@gmail.com>
2023-12-27 15:07:11 +0000

    feat: Add art logo. Add a while loop for the play again feature. Fix the problem of out of range with the use of modulus operation.

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
 
-def caesar(direction, text, shift):
-    cypher_text = ''
-    if direction == 'decode':
-        shift *= -1
-    for letter in text:
-        letter_position = alphabet.index(letter)
-        cypher_text += alphabet[letter_position + shift]
-    print(f"The {direction} message is: {cypher_text}")
-caesar(direction, text, shift)
+print(logo)
+play_again = True
+
+while play_again == True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    #modulus to set the range
+    shift = shift % 26
+
+    def caesar(direction, text, shift):
+        cypher_text = ''
+        if direction == 'decode':
+            shift *= -1
+        for letter in text:
+            letter_position = alphabet.index(letter)
+            cypher_text += alphabet[letter_position + shift]
+        print(f"The {direction} message is: {cypher_text}\n")
+
+    caesar(direction, text, shift)
+    play_again = input("Type 'Yes' if you want to go again. Otherwise type 'no'").lower()
+    if play_again == 'yes':
+        play_again = True
+
